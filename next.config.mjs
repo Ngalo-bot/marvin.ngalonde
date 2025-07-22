0d5ec3fc6e852deae98d3ee62402bb88b24baf0a
verdsoft <verdsoft@gmail.com>
2025-07-22 17:33:16 +0200

    another one

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+const repo = 'https://github.com/Ngalo-bot/marvin.ngalonde.git';
+
 const nextConfig = {
+  output: 'export', 
+
+  basePath: `/${repo}`,         
+  assetPrefix: `/${repo}/`,
   eslint: {
     ignoreDuringBuilds: true,
   },
